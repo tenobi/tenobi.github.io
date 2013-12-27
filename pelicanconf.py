6b5d2d8011d3fde36cc62fffd973dc221d32d637
tenobi <tenobi@openmailbox.org>
2013-12-28 00:58:07 +0800

    update pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -16,14 +16,14 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
 # Blogroll
-LINKS =  (('Pelican', 'http://getpelican.com/'),
-          ('Python.org', 'http://python.org/'),
-          ('Jinja2', 'http://jinja.pocoo.org/'),
-          ('You can modify those links in your config file', '#'),)
+#LINKS =  (('Pelican', 'http://getpelican.com/'),
+#          ('Python.org', 'http://python.org/'),
+#          ('Jinja2', 'http://jinja.pocoo.org/'),
+#          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('last.fm', 'http://last.fm/user/t3n0b1'),
+          ('Pearltrees', 'http://www.pearltrees.com/tenobi'),)
 
 DEFAULT_PAGINATION = 10
 
